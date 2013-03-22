dbfcb0c15382e6dcdcf7f29e5af7cb3a2cafa8d0
Sam Halliday <sam.halliday@gmail.com>
2013-03-22 13:55:34 +0000

    close #24

--- server/core/src/main/scala/org/cakesolutions/akkapatterns/core/application.scala
+++ server/core/src/main/scala/org/cakesolutions/akkapatterns/core/application.scala
@@ -1,6 +1,7 @@
 package org.cakesolutions.akkapatterns.core
 
 import akka.actor.{ActorLogging, Props, Actor}
+import akka.routing.FromConfig
 
 case class GetImplementation()
 case class Implementation(title: String, version: String, build: String)
@@ -22,9 +23,12 @@ class ApplicationActor extends Actor with ActorLogging {
       sender ! Implementation(title, version, build)
 
     case Start() =>
-      val messageDelivery = context.actorOf(Props[MessageDeliveryActor], "messageDelivery")
-      context.actorOf(Props(new CustomerActor(messageDelivery)), "customer")
-      context.actorOf(Props(new UserActor(messageDelivery)),     "user")
+      val messageDelivery = context.actorOf(
+        Props[MessageDeliveryActor].withRouter(FromConfig()).withDispatcher("low-priority-dispatcher")
+        , "messageDelivery"
+      )
+      context.actorOf(Props(new CustomerActor(messageDelivery)).withRouter(FromConfig()), "customer")
+      context.actorOf(Props(new UserActor(messageDelivery)).withRouter(FromConfig()),     "user")
 
 
     case Stop() =>
