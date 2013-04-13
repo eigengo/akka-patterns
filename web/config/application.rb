ca00c2945e2e72eacc48bae1854894f2cd136ff8
Jan Machacek <janm@cakesolutions.net>
2013-04-13 17:17:17 +0100

    Rails light

--- web/config/application.rb
+++ web/config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
 
 # Assets should be precompiled for production (so we don't need the gems loaded then)
 Bundler.require(*Rails.groups(assets: %w(development test)))
--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -13,15 +13,6 @@ Web::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations
-  config.active_record.migration_error = :page_load
-
   # Debug mode disables concatenation and preprocessing of assets.
   config.assets.debug = true
 end
